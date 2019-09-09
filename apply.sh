ab7caba9393d9a947867dac93e6f8c516fc8fccd
Ben Hecht <hechtb3@gmail.com>
2019-09-09 11:19:18 -0400

    cat *.yaml instead of relying on wildcard in kubectl

--- apply.sh
+++ apply.sh
@@ -5,4 +5,4 @@ kubectl config set-credentials cluster --token "${KUBETOKEN}"
 kubectl config set-context cluster --cluster cluster --user cluster --namespace "${PLUGIN_NAMESPACE}"
 kubectl config use-context cluster
 
-kubectl apply -f *.yaml
+cat *.yaml | kubectl apply -f -

61a4e595e2b67da6fd5cb569b5764475f7f4eb2d
Ben Hecht <ben@Macintosh.local>
2021-03-05 21:45:19 -0500

    Replace Drone vars in YAML files

--- apply.sh
+++ apply.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-env
+ENV_VAR_KEY_ARRAY=()
+
+for var in "${!DRONE_@}"; do
+    ENV_VAR_KEY_ARRAY+=("\${$var}")
+done
+
+ENV_VAR_KEYS=$(IFS=" "; echo "${ENV_VAR_KEY_ARRAY[*]}")
+
+for filename in *.yaml; do
+    envsubst "$ENV_VAR_KEYS" < $filename > $filename.tmp
+    mv "$filename.tmp" "$filename"
+done
+
+kubectl config set-cluster cluster --server "${PLUGIN_SERVER}"
+kubectl config set-credentials cluster --token "${KUBETOKEN}"
+kubectl config set-context cluster --cluster cluster --user cluster --namespace "${PLUGIN_NAMESPACE}"
+kubectl config use-context cluster
+
+cat *.yaml | kubectl apply -f -

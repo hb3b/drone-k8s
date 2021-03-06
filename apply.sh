db7c23d6d3c6b32d736775dcfeaad5c872e267cc
Ben Hecht <ben@Macintosh.local>
2021-03-05 20:43:04 -0500

    Testing

--- apply.sh
+++ apply.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-kubectl config set-cluster cluster --server "${PLUGIN_SERVER}"
-kubectl config set-credentials cluster --token "${KUBETOKEN}"
-kubectl config set-context cluster --cluster cluster --user cluster --namespace "${PLUGIN_NAMESPACE}"
-kubectl config use-context cluster
-
-cat *.yaml | kubectl apply -f -
+env
